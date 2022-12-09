2b44a81511590cfc9ea10eb60e2798f3666ff1fa
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2022-12-09 13:32:20 +0100

    Gmail service

--- gc_google_services_api/gmail/__init__.py
+++ gc_google_services_api/gmail/__init__.py
@@ -1,58 +1,48 @@
 from __future__ import print_function
 
 import base64
-import json
 import os
 
 from email.message import EmailMessage
-from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# TODO: Remove default value (Just development prupose)
-AUTHENTICATION_EMAIL = os.getenv('AUTHENTICATION_EMAIL')
-CREDENTIALS_BASE64 = os.getenv('GOOGLE_SHEET_CREDENTIALS', '')
+from gc_google_services_api.auth import Auth
+
+AUTHENTICATION_EMAIL = os.getenv('AUTHENTICATION_EMAIL', '')
+
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.send',
 ]
 
-try:
-    CREDENTIALS_CONTENT = json.loads(base64.b64decode(CREDENTIALS_BASE64))
-except json.JSONDecodeError as e:
-    print('[ERROR CREDENTIALS_CONTENT]: ', e)
-    CREDENTIALS_CONTENT = ''
 
+class Gmail:
+    def __init__(self, subject_email) -> None:
+        self.credentials = Auth(SCOPES, subject_email).get_credentials()
+        self.service = build('gmail', 'v1', credentials=self.credentials)
 
-def send_email():
-    credentials = service_account.Credentials.from_service_account_info(
-        CREDENTIALS_CONTENT,
-        scopes=SCOPES)
+    def send_email(self, email_message, email_subject, to=[]):
+        try:
+            message = EmailMessage()
 
-    creds = credentials.with_subject(AUTHENTICATION_EMAIL)
+            message.set_content(email_message)
 
-    try:
-        service = build('gmail', 'v1', credentials=creds)
-        message = EmailMessage()
+            message['to'] = to
+            message['from'] = AUTHENTICATION_EMAIL
+            message['subject'] = email_subject
 
-        message.set_content('This is automated draft mail')
+            # encoded message
+            encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+                .decode()
 
-        message['to'] = AUTHENTICATION_EMAIL
-        message['from'] = AUTHENTICATION_EMAIL
-        message['subject'] = 'Automated draft'
+            create_message = {
+                'raw': encoded_message
+            }
+            send_message = (self.service.users().messages().send
+                            (userId='me', body=create_message).execute())
+            print(F'Message Id: {send_message["id"]}')
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            send_message = None
 
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
-            .decode()
-
-        create_message = {
-            'raw': encoded_message
-        }
-        # pylint: disable=E1101
-        send_message = (service.users().messages().send
-                        (userId='me', body=create_message).execute())
-        print(F'Message Id: {send_message["id"]}')
-    except HttpError as error:
-        print(F'An error occurred2: {error}')
-        send_message = None
-
-    return send_message
+        return send_message
--- gc_google_services_api/permissions/workspace.py
+++ gc_google_services_api/permissions/workspace.py
@@ -1,5 +1,4 @@
 from googleapiclient.discovery import build
-from pprint import pprint
 
 from gc_google_services_api.auth import Auth
 
