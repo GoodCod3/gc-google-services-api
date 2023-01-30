551ec16c0bc0ec362d92e25dd4356cacf3ebc8d2
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2023-01-30 12:23:48 +0100

    Fixing setup

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ setup(
     name='gc_google_services_api',
     version=current_version,
     packages=find_packages(),
-    data_files=[('gc_google_services_api', ['*.json'])],
-
     author='Jonathan Rodriguez Alejos',
     author_email='jrodriguez.5716@gmail.com',
     install_requires=open('requirements.txt').read().splitlines(),
